34103772cc42508a70c5a5e4425b94644179e02e
Volodymyr Savranskyi <detlassnake@gmail.com>
2019-11-10 11:05:45 +0200

    Adding new class Dispatcher

---
+++ src/main/Dispatcher.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Scanner;
+
+public class Dispatcher  {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = (int)(Math.random() * 1000);
+        int input = 0;
+        int count = 0;
+
+        System.out.println("Guess number between 1 and 1000");
+        System.out.println("Answer " + number);
+        do {
+            input = in.nextInt();
+            count++;
+            if (input > number) {
+                System.out.println("Too high");
+            } else if (input == number){
+                System.out.println("Yoi win correct number is " + number + ", number of guesses it took to get the correct number is " + count);
+            } else {
+                System.out.println("Too low");
+            }
+        } while (number != input);
+    }
+}
